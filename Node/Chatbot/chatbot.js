cfe199cce386177fc7010aa56046bf85abf3678e
Najd Feki <najd.feki@esprit.tn>
2021-03-17 03:36:11 +0100

    started storing data to save them in the db

--- Node/Chatbot/chatbot.js
+++ Node/Chatbot/chatbot.js
@@ -1,9 +1,12 @@
 "use strict";
 const dialogflow = require("dialogflow");
-const { func } = require("joi");
 const config = require("../config/keys");
 const sessionCLient = new dialogflow.SessionsClient();
 const sessionPath = sessionCLient.sessionPath(config.googleProjectID, config.dialogFlowSessionID);
+// user attributes list///
+var name;
+var age;
+////////
 module.exports = {
   textQuery: async (text, parameters = {}) => {
     let self = module.exports;
@@ -28,6 +31,17 @@ module.exports = {
     return responses;
   },
   handleAction: function (responses) {
+    let self = module.exports;
+    let queryResult = responses[0].queryResult;
+    let fields = queryResult.parameters.fields;
+    let key = Object.keys(fields)[0];
+    if (key == "age") age = fields.age.structValue.fields.amount.numberValue;
+    if (key == "person") name = fields.person.structValue.fields.name.stringValue;
+    //if (key == "")
+    let res = self.saveUser();
     return responses;
+  },
+  saveUser: function () {
+    console.log("name is : " + name + "age is " + age);
   },
 };
