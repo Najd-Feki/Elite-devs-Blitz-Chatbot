f26cb1054f8d03281745ae11bbcc5a19d25c0188
omar <omar.fehri@esprit.tn>
2021-04-21 05:05:16 +0100

    reclamation

--- Client/blitz/src/pages/Reclamation.js
+++ Client/blitz/src/pages/Reclamation.js
@@ -1,35 +1,74 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import Header from "components/headers/light.js";
-
 import '../assets/reclamation/reclamation.css'
+import axios from "axios";
+
 export default () => {
+  
+     const [reclamation,setReclamation]=useState({userId:'',type:'',description:'',date:Date.now()})
+     const [submitted, setSubmitted] = useState(false);
+   
+     const handleSubmit = (e) => {
+      e.preventDefault();
+     axios.post('http://localhost:5000/reclamation/add',reclamation)
+     .then(setReclamation({userId:'',type:'',description:'',date:Date.now()}))
+     .then(alert('Clain successfully sent'))
+    };
+  
+     const handleUserIdInputChange = (event) => {
+      event.persist();
+      setReclamation((reclamation) => ({
+        ...reclamation,
+        userId: event.target.value,
+      }));
+    };
+
+    const handleTypeInputChange = (event) => {
+      event.persist();
+      setReclamation((reclamation) => ({
+        ...reclamation,
+        type: event.target.value,
+      }));
+    };
+    
+    const handledescriptionInputChange = (event) => {
+      event.persist();
+      setReclamation((reclamation) => ({
+        ...reclamation,
+        description: event.target.value,
+      }));
+    };
+
+
+
     return (
       <>
         <Header />
-        <div class="container">
+        <div className="reclamation-container reclamation-body">
   <h2>Here you can post our complaints</h2>
   <h1>send us </h1>
   
-  <form action="" >
-    <div class="fields">
-    <p>Name</p>
+  <form action="" className='reclamation-form' >
+    <div className="reclamation-fields">
+    <p className="reclamation-p">id</p>
       <span>
-       <input placeholder="Name" type="text" />
+       <input placeholder="Id" type="text" value={reclamation.userId} onChange={handleUserIdInputChange} className='reclamation-input'/>
     </span>
     <br />
-    <p>Subject</p>
+    <p className="reclamation-p">Subject</p>
      <span>
-       <input placeholder="type" type="type" />
+       <input placeholder="type" type="type" value={reclamation.type} onChange={handleTypeInputChange} className='reclamation-input' />
     </span>
     <br />
     <span>
-        <p>your complaint</p>
-       <textarea placeholder="description" type="description" rows="4" cols="50" />
+        <p className="reclamation-p">your complaint</p>
+       <textarea placeholder="description" type="description" rows="4" cols="50" value={reclamation.description} onChange={handledescriptionInputChange}  />
     </span>
     </div>
-    <div class="submit">
-      <input class="submit" value="send" type="button" />
+    <div className="submit">
+      <input className="submit" value="send" type="button" onClick={handleSubmit} className='reclamation-input'/>
     </div>
+   
   </form>
 </div>
         
