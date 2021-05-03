d15d7a050210ca4f2067e4f30907d76bfefc62f2
Hrairi Mehdi <mehdi.hrairi@esprit.tn>
2021-05-03 06:55:57 +0100

    redirect links

--- Client/blitz/src/components/recommandation/CourseRec.js
+++ Client/blitz/src/components/recommandation/CourseRec.js
@@ -11,11 +11,16 @@ const contentStyle = {
 
 const CourseRec = ({recData}) => {
     if(recData){console.log(recData);}
+    const openInNewTab = (c) => {
+        const newWindow = window.open("https://www.udemy.com" + c, "_blank", "noopener,noreferrer");
+        console.log(c.url);
+        if (newWindow) newWindow.opener = null;
+      };
     const display = recData.map((a) => {
         return  (
-            <div>
-                <img src={a.image_480x270} alt={a.title} style={{height:"20rem"}} />
-            <h3  style={{height: '80px',
+            <div >
+                <img src={a.image_480x270} alt={a.title} style={{height:"20rem"}}  />
+            <h3 onClick={()=>openInNewTab(a.url)} style={{height: '80px',
                 color: '#fff',
                 lineHeight: '60px',
                 textAlign: 'center',
