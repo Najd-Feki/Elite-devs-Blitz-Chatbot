2e55139776afea5083b8e4c607c29287b05e0473
yessine47 <75212246+yessine47@users.noreply.github.com>
2021-05-05 02:56:51 +0200

    add event api + fixed profile update

--- Client/blitz/src/pages/EventDetail.js
+++ Client/blitz/src/pages/EventDetail.js
@@ -4,18 +4,18 @@ import Header from "../components/headers/light";
 import Footer from "components/footers/SimpleFooter";
 import axios from 'axios';
 import Button from '@material-ui/core/Button';
-
 import '../assets/eventcss/event.css'
-export default function EventDetail() {
-const [event,setEvent] = useState({}); 
 
+export default function EventDetail() {
+const [event,setEvent] = useState({});
 const {id}=useParams()
 useEffect(() => {
-axios.get('http://localhost:5000/event/'+id).then(Response =>{
-setEvent(Response.data);
-})
+    axios.get('https://api.predicthq.com/v1/events/?id='+id, {
+      headers: { Authorization: `Bearer B5pmjnvZc_pAMp8-MFtAVKHcldB45VFhitROU_n5` }
+    })      .then(function (res) {
+        setEvent(res.data.results[0]);
+      })
 },[])
-
     return (
       <>
     <Header />
@@ -25,22 +25,29 @@ setEvent(Response.data);
         <section className="tm-mb-1" id="about">
             <div className="tm-row tm-about-row">
                 <div className="tm-section-1-l">
-                <img src={event.photo} alt="About image" class="tm-img-responsive"/>
-
+                <div className="mapouter">
+                	<div className="gmap_canvas">
+                    	<iframe width="600" height="500" id="gmap_canvas" src={"https://maps.google.com/maps?q="+event.timezone+"&t=&z=13&ie=UTF8&iwloc=&output=embed"} frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+                    </div>
+                </div>
                 </div>
                 <article className="tm-section-1-r tm-bg-color-8">
-                    <h2 className="tm-mb-2 tm-title-color">{event.name}</h2>
-                    <p>{event.subject}</p> 
-                    <p style={{color:"darkblue"}}>Join us at {event.date?.slice(0,10)} in {event.location}</p>       
-                    <Button style={{left: "35%"}} variant="contained" color="secondary"> Join event</Button>
+                    <h2 className="tm-mb-2 tm-title-color">{event.title}</h2>
+                    <p></p>
+                    <p style={{color:"darkblue"}}>Description : {event.description}</p>
+                    <p style={{color:"darkblue"}}>Categorys :  {event.labels}</p>
+                    <p style={{color:"darkblue"}}>This event start at : {event.start?.slice(0,10)}</p>
+                    <p style={{color:"darkblue"}}>This event end at : {event.start?.slice(0,10)}</p>
+                    <p style={{color:"darkblue"}}>Duration : {event.duration}</p>
+                    <p style={{color:"darkblue"}}>Location : {event.timezone}</p>
+                    <Button style={{left: "35%"}} variant="contained" color="secondary">Notify me</Button>
                 </article>
             </div>
         </section>
-    </div>    
-    <Footer />    
+    </div>
+    <Footer />
 </div>
-
       </>
-    );
-}
 
+    )
+}
